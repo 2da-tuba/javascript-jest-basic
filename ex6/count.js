d43764fe83f6a00c1abc6dcbd184192be68ced1c
2da-tuba <hidetubatsuda@gmail.com>
2022-05-24 16:01:50 +0900

    完成

--- ex6/count.js
+++ ex6/count.js
@@ -4,5 +4,26 @@
  * @returns {string} 各数字の出現数を表す文字列。例えば"1(3) 3(3)"
  */
 function count(inputString) {
-    return '';
+    let arr = inputString.split(' ');
+    let obj = {};
+    let sumstr = '';
+    
+
+    for(let i = 0; i < arr.length; i++){
+        if(obj.hasOwnProperty(arr[i])){
+            obj[arr[i]] += 1;
+        }else{
+            obj[arr[i]] = 1;//ここでオブジェクトに新しくキーとバリューを追加。
+        }
+    }
+
+    let strtoarr = Array.from(new Set(arr));
+    
+    let n = [];
+    for(let i = 0; i < strtoarr.length; i++){
+        n.push(strtoarr[i] + "(" + Object.values(obj)[i] + ")");
+    }
+
+    return n.join(' ');
 }
+module.exports = count;
